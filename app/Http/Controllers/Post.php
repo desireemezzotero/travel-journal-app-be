40f0cc9e91fb530fbaa416f666f590799e318be1
desireemezzotero <desireemezzotero1@gmail.com>
2025-08-30 17:34:09 +0200

    feat: Add authentication and profile management features

--- app/Http/Controllers/Post.php
+++ app/Http/Controllers/Post.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mood;
+use App\Models\Post as ModelsPost;
 use Illuminate\Http\Request;
 
 class Post extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = ModelsPost::with('moods', 'tags', 'photos')->get();
+
+        return view('welcome', compact('posts'));
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,57 @@
-@extends('layout/master')
+@extends('layout.master')
 
-@section('component')
-<div class="bg-black min-h-screen">
-<h1>ciao</h1>
+@section('content')
+
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">Stato</th>
+                <th scope="col" class="px-6 py-3">Città</th>
+                <th scope="col" class="px-6 py-3">Descrizione</th>
+                 <th scope="col" class="px-6 py-3">mood</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($posts as $post)
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $post->title }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $post->location }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $post->description }}
+                </td>
+                <td class="px-6 py-4">
+                  @foreach ($post->moods as $mood)
+                   <p>{{$mood->title }}</p> 
+                  @endforeach 
+                </td>
+
+                <td class="px-6 py-4">
+                  @foreach ($post->tags as $tag)
+                   <p>{{$tag->name }}</p> 
+                  @endforeach 
+                </td>
+
+                 <td class="px-6 py-4">
+                  @foreach ($post->photos as $photo)
+                   <p>{{$photo->type }}</p> 
+                  @endforeach 
+                </td>
+
+                
+            </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
 </div>
+
+
 @endsection
